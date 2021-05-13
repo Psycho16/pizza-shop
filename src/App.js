7b3953b9979702fe155830146b018de04fa31acb
Konstantin <lobanov2000k@yandex.ru>
2021-05-13 17:35:15 +0500

    pass pizzas props to PizzaBlock component

--- src/App.js
+++ src/App.js
@@ -5,15 +5,21 @@ import { Header } from './components/index';
 import { Home, Cart } from './pages/index';
 
 function App() {
+  const [pizzas, setPizzas] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch('http://localhost:3000/db.json')
+      .then((resp) => resp.json())
+      .then((json) => {
+        setPizzas(json.pizzas);
+      });
+  }, []);
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
-        <Route exact path="/" component={Home} />
+        <Route exact path="/" render={() => <Home items={pizzas} />} />
         <Route exact path="/cart" component={Cart} />
-        {/* <Link exact path="/" component={Home} /> */}
-        {/* <Home /> */}
-        {/* <Cart /> */}
       </div>
     </div>
   );
