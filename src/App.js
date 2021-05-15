ae6989e463e04b39ebc9ad76f1ed84bde19b6d4f
Konstantin <lobanov2000k@yandex.ru>
2021-05-15 22:07:29 +0500

    add axios

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from './assets/img/pizza-logo.svg';
+import axios from 'axios';
 import { Route } from 'react-router-dom';
 import { Header } from './components/index';
 import { Home, Cart } from './pages/index';
@@ -8,11 +9,14 @@ function App() {
   const [pizzas, setPizzas] = React.useState([]);
 
   React.useEffect(() => {
-    fetch('http://localhost:3000/db.json')
-      .then((resp) => resp.json())
-      .then((json) => {
-        setPizzas(json.pizzas);
-      });
+    axios.get('http://localhost:3000/db.json').then(({ data }) => {
+      setPizzas(data.pizzas);
+    });
+    // fetch('http://localhost:3000/db.json')
+    //   .then((resp) => resp.json())
+    //   .then((json) => {
+    //     setPizzas(json.pizzas);
+    //   });
   }, []);
   return (
     <div className="wrapper">
